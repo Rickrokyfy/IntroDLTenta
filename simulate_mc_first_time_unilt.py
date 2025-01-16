57d844f3b2554c5b33bd80765d47ebe77a17c212
lokl4297 <love.klockars.4297@student.uu.se>
2025-01-16 12:14:25 +0100

    addded comments

--- simulate_mc_first_time_unilt.py
+++ simulate_mc_first_time_unilt.py
@@ -5,8 +5,10 @@ matrix = [[0.5,0.5,0],[0.5,0,0.5],[0.5,0,0.5]]
 #Switch up matrix so that we cycle on the value that we want to predict first time until we enter
 matrix = [[0.5,0.5,0],[0.5,0,0.5],[0,0,1]]
 
+state_to_be_reached = 2
+
 runprog = True
-condition = [np.longdouble(1),np.longdouble(0),np.longdouble(0)]
+condition = [np.longdouble(1),np.longdouble(0),np.longdouble(0)] # where we start
 estimate = np.longdouble(0)
 prevprob =np.longdouble(0)
 iter = np.longdouble(0)
@@ -14,10 +16,10 @@ np.longdouble
 while(runprog):
     #Calculate new condition
     #
-    estimate += iter*(condition[2]-prevprob)
-    if(condition[2]-prevprob < np.longdouble(0.000000000000000001) and iter > 10):
+    estimate += iter*(condition[state_to_be_reached]-prevprob) # byt condition till exp state
+    if(condition[state_to_be_reached]-prevprob < np.longdouble(0.000000000000000001) and iter > 10):
         runprog = False
-    prevprob = condition[2]
+    prevprob = condition[state_to_be_reached]
     iter += 1
     condition = np.dot(condition, matrix)
         
